c1aff8eda56d30913715b7660dd632f444d6b9a2
macbook <romes57@gmail.com>
2023-04-14 11:22:56 +0300

    alpha version

--- smu88/smu88/settings.py
+++ smu88/smu88/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', default='django-insecure-w@trop!hx_fg!m0m!c8ao(n%g55^9foo2j3($fkc8mwxw#)u+%')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
